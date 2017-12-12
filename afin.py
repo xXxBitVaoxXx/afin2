4e42dae1d857c8b8bbd7ab119b68018d3b21b83a
xXxBitVaoxXx <34238587+xXxBitVaoxXx@users.noreply.github.com>
2017-12-13 00:21:44 +0700

    Update afin.py

--- afin.py
+++ afin.py
@@ -1,13 +1,12 @@
 from math import gcd
 
-
 def cipher():
 	alphabet = 'qwertyuiopasdfghjklzxcvbnmйцукёенгшщзхъфывапролджэячсмитьбю'
 	alphabet = ''.join(sorted(set(alphabet)))
 	length = len(alphabet)
+	
 	res = ''
-	print(alphabet)
-	print(len(alphabet))
+	print('Мощность алфавита: ', len(alphabet))
 
 	with open("otext.txt", 'r') as l:
 		text = l.read()
@@ -17,11 +16,11 @@ def cipher():
 	print(' '.join(map(str,al)))
 
 	alpha = int(input("Выбирите альфа: "))
+
 	while alpha not in al:
 		alpha = int(input("Выберете альфа: "))
 
 	betta = int(input("Выбирите бетта: "))
-	
 	for char in text:
 		num = alphabet.find(char)
 		y = (alpha * num + betta) % length
@@ -34,7 +33,39 @@ def cipher():
 			f.write(res[char])	
 
 
+def dcipher():
+	
+	res = ''
+	alphabet = 'qwertyuiopasdfghjklzxcvbnmйцукёенгшщзхъфывапролджэячсмитьбю'
+	alphabet = ''.join(sorted(set(alphabet)))
+	length = len(alphabet)
+	
+	with open('ctext.txt') as f:
+		ctext = f.read()
+		ctext = ctext.lower().rstrip()
+	
+	al = [alpha for alpha in range(1, length) if gcd(length, alpha) == 1]
+	print(' '.join(map(str,al)))
 
+	alpha = int(input("Выбирите альфа: "))
+	while alpha not in al:
+		alpha = int(input("Выберете альфа: "))
+
+	betta = int(input("Выбирите бетта: "))
+
+	for char in ctext:
+		num = alphabet.find(char)
+		y = ((alpha ** -1) * (num - betta)) % length
+		res += alphabet[y]
+	print(res)
+	
+	with open('otext', 'w') as f:
+		for char in range(len(ctext)):
+			f.write(res[char])
+	
 		
-
-cipher()
+a = input('Вы хотите "Зашифровать" текст или "Расшифровать"?\n')
+if a == 1:
+	cipher()
+else:
+	dcipher()
